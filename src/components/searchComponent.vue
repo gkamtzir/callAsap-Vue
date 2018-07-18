d61168f2db70926f249431a790502926b2923052
George Kamtziridis <georgekam96@gmail.com>
2018-07-18 10:13:19 +0300

    Alphabetized country list

--- src/components/searchComponent.vue
+++ src/components/searchComponent.vue
@@ -94,7 +94,27 @@
             self.getCountries()
                 .then(function(response) {
 
-                    self.countries = response.body;
+                    let countries = response.body;
+
+                    countries.sort(function(a, b) {
+
+                        if (a.Name < b.Name) {
+
+                            return -1;
+
+                        } else if (a.Name > b.Name) {
+
+                            return 1;
+
+                        } else {
+
+                            return 0;
+
+                        }
+
+                    });
+
+                    self.countries = countries;
 
                 },
                 function(error) {
